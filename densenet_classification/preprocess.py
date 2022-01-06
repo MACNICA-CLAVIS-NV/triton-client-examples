4f4afe5633c683c3db6a0ec1702a0057b8d180b8
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2022-01-06 11:51:01 +0900

    Changed comments

--- densenet_classification/preprocess.py
+++ densenet_classification/preprocess.py
@@ -1,17 +1,50 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2022 MACNICA-CLAVIS-NV
+
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 import numpy as np
 from PIL import Image
 import tritonclient.grpc.model_config_pb2 as mc
 from tritonclient.utils import triton_to_np_dtype
 
 
+# Quoted from the following article.
+# https://qiita.com/derodero24/items/f22c22b22451609908ee
 def cv2pil(image):
-    ''' OpenCV型 -> PIL型 '''
+    ''' OpenCV -> PIL '''
     new_image = image.copy()
-    if new_image.ndim == 2:  # モノクロ
+    if new_image.ndim == 2:  # monochrome
         pass
-    elif new_image.shape[2] == 3:  # カラー
+    elif new_image.shape[2] == 3:  # color
         new_image = new_image[:, :, ::-1]
-    elif new_image.shape[2] == 4:  # 透過
+    elif new_image.shape[2] == 4:  # transparent
         new_image = new_image[:, :, [2, 1, 0, 3]]
     new_image = Image.fromarray(new_image)
     return new_image
--- tiny_yolov2/preprocess.py
+++ tiny_yolov2/preprocess.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 
-# MIT License
-#
-# Copyright (c) 2021 MACNICA-CLAVIS-NV
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Copyright (c) 2022 MACNICA-CLAVIS-NV
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 import numpy as np
@@ -29,14 +35,16 @@ import tritonclient.grpc.model_config_pb2 as mc
 from tritonclient.utils import triton_to_np_dtype
 
 
+# Quoted from the following article.
+# https://qiita.com/derodero24/items/f22c22b22451609908ee
 def cv2pil(image):
-    ''' OpenCV型 -> PIL型 '''
+    ''' OpenCV -> PIL '''
     new_image = image.copy()
-    if new_image.ndim == 2:  # モノクロ
+    if new_image.ndim == 2:  # monochrome
         pass
-    elif new_image.shape[2] == 3:  # カラー
+    elif new_image.shape[2] == 3:  # color
         new_image = new_image[:, :, ::-1]
-    elif new_image.shape[2] == 4:  # 透過
+    elif new_image.shape[2] == 4:  # transparent
         new_image = new_image[:, :, [2, 1, 0, 3]]
     new_image = Image.fromarray(new_image)
     return new_image
